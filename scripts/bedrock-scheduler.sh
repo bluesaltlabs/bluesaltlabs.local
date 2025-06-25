8dccb7f8449d67349a6c232ab152f4a11f846368
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-06-25 14:53:46 -0700

    Adds initial bedrock scheduler script.

--- scripts/bedrock-scheduler.sh
+++ scripts/bedrock-scheduler.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-# Run this hourly
+#REPO_URL="https://gitea.bluesaltlabs.com/BlueSaltLabs/bedrock"
+#REPO_URL="git@gitea.bluesaltlabs.com:BlueSaltLabs/bedrock.git"
+REPO_URL="git@docker-server.local:BlueSaltLabs/bedrock.git"
+REPO_PATH="/scraper"
+WORK_DIR="/root/projects/bedrock"
+
 
 # 1. Output script start time to log.
+echo "Starting Bedrock Scheduler..."
 
-
-# 2. Ensure docker is running. If it is not, log a failed attempt.
-
+# 2. Ensure docker is running. If it is not, log and exit
+if ! docker info >/dev/null 2>&1; then
+  echo "Docker is not running! exiting."
+  exit 1
+fi
 
 # 3. Pull scraper code repo files (to ensure updates are added automatically).
-
+# The commented out parts of this only had to run the first time
+cd "$WORK_DIR"
+#git init
+#git remote add origin "$REPO_URL"
+#git config core.sparseCheckout true
+#echo "$REPO_PATH/*" > .git/info/sparse-checkout
+git pull origin main
 
 # 4. Build docker container (use docker compose to simplify?).
 #    Note: if there haven't been any changes, this part shouldn't need to do anything.
-
+docker build -t bedrock-scraper .
 
 # 5. Run the docker container
-
+docker run bedrock-scraper
 
 # 6. output  that the script has completed.
+echo "Bedrock Scheduler run Complete."

72cefbe1ec6fbdb2adf5f4d1299251eface740ca
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-06-25 14:18:03 -0700

    Adds initial bedrock scheduler script.

---
+++ scripts/bedrock-scheduler.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Run this hourly
+
+# 1. Output script start time to log.
+
+
+# 2. Ensure docker is running. If it is not, log a failed attempt.
+
+
+# 3. Pull scraper code repo files (to ensure updates are added automatically).
+
+
+# 4. Build docker container (use docker compose to simplify?).
+#    Note: if there haven't been any changes, this part shouldn't need to do anything.
+
+
+# 5. Run the docker container
+
+
+# 6. output  that the script has completed.
---
+++ scripts/hourly.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/root/scripts/bedrock-scheduler.sh

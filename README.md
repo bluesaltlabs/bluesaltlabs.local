b74c2fbf144cae516b65e175477630cf7b43e5ad
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-07-21 08:56:31 -0700

    Updates restic backup script to backup to usb-drive and windows-server locations. Adds docker config file.

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ This repository is the 'infrustructure as code' repository for `bluesaltlabs.loc
 This repository is intended to be deployed to `docker-server`.
 
 This is my effort to finally be free of my dependance on cloud services.
+
+
+---
+
+## Notes
+
+- `daemon.json` belongs at `/etc/docker/daemon.json`.
+
+---
--- scripts/backup-docker-server.sh
+++ scripts/backup-docker-server.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
 # Set environment variables
-export RESTIC_REPOSITORY="/mnt/windows-server/restic"
+export USB_DRIVE_REPO="/mnt/usb-drive/restic"
+export WINDOWS_SERVER_REPO="/mnt/windows-server/restic"
 export RESTIC_PASSWORD="your-repo-password" # todo: actually use an environment variable.
+
 
 # Stop the gitea service
 echo "Stopping gitea service..."
 systemctl stop gitea
 
+
 # Stop the docker service
 echo "Stopping docker service..."
 systemctl stop docker
 
+
 # Back up docker data, excluding images
-echo "Backing up docker data..."
-restic backup /var/lib/docker --exclude /var/lib/docker/images/*
+echo "Backing up docker data to usb-drive..."
+restic backup /home/docker --repo $USB_DRIVE_REPO --exclude /home/docker/images/*
+
+echo "Backing up docker data to windows-server..."
+restic backup /home/docker --repo $WINDOWS_SERVER_REPO --exclude /home/docker/images/*
+
 
 # Back up gitea data
-echo "Backing up gitea data..."
-restic backup /var/lib/gitea
+echo "Backing up gitea data to usb-drive..."
+restic backup /var/lib/gitea --repo $USB_DRIVE_REPO
+
+echo "Backing up gitea data to windows-server..."
+restic backup /var/lib/gitea --repo $WINDOWS_SERVER_REPO
+
 
 # prune old snapshots
-echo "Pruning old snapshots..."
-restic forget --keep-last 7 --prune
+echo "Pruning old snapshots from usb-drive..."
+restic forget --keep-last 7 --prune --repo $USB_DRIVE_REPO
+
+echo "Pruning old snapshots from windows-server..."
+restic forget --keep-last 7 --prune --repo $WINDOWS_SERVER_REPO
+
 
 # Restart the docker service
 echo "Restarting docker service..."
 systemctl start docker
+
 
 # Restart the gitea service
 echo "Restarting gitea service..."
